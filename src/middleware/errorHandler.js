14f3794f966a528a9f575a2b994d3440156db92e
Kateryna Havryna <khavryna@gmail.com>
2026-04-02 22:03:25 +0200

    init mongodb project

---
+++ src/middleware/errorHandler.js
@@ -0,0 +1,13 @@
+export const errorHandler = (
+  err,
+  req,
+  res,
+  next,
+) => {
+  const status = err.status || 500;
+
+  res.status(status).json({
+    message:
+      err.message || "Internal Server Error",
+  });
+};
---
+++ src/server.js
@@ -0,0 +1,39 @@
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+
+import { connectMongoDB } from "./db/connectMongoDB.js";
+import notesRoutes from "./routes/notesRoutes.js";
+
+import { logger } from "./middleware/logger.js";
+import { notFoundHandler } from "./middleware/notFoundHandler.js";
+import { errorHandler } from "./middleware/errorHandler.js";
+
+dotenv.config();
+
+const app = express();
+
+// 🔌 MongoDB connection
+await connectMongoDB();
+
+// 🧩 Middleware
+app.use(logger);
+app.use(cors());
+app.use(express.json());
+
+// 🛣 Routes
+app.use(notesRoutes);
+
+// ❌ 404
+app.use(notFoundHandler);
+
+// ⚠️ Errors
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(
+    `🚀 Server running on port ${PORT}`,
+  );
+});
